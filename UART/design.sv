ccd4cdcdc1091e75f57b0499861b778e9367b2a6
Jan <9010650+goyf@users.noreply.github.com>
2019-09-05 12:50:35 +0100

    UART: Switched from if statements to case for better clarity. Added 2 stop bauds. To do: Idle function and a transaction class/function.

--- UART/design.sv
+++ UART/design.sv
@@ -2,6 +2,10 @@ module uart(
                             input logic rx,
                             output logic tx
                                             );
+    
+    typedef enum {IDLE, START, DATA, PAR, STOP} uart_states;
+    uart_states state;
+    
     logic data_arr[8:0];
     int data_counter = 0;
     logic start_baud = 0;
@@ -11,23 +15,34 @@ module uart(
     
     //initial begin
     always @ (rx) begin
-        //assign tx = rx;
-        //assign curr_rx = rx;
-        if (start_baud == 1) begin
-            assign tx = rx; 
-            data_counter = data_counter + 1;
+        
+        case (state)
+        
+            IDLE : begin
+                    if (rx == 0) begin
+                        state = DATA;
+                    end 
+                end
+            DATA : begin
+                    assign tx = rx; 
+                    data_counter = data_counter + 1;
             
-            if (data_counter == 16) begin
-                start_baud = 0;
-                data_counter = 0;
-            end
-            //assign start_baud = 0;
-        end
-        else begin
-            if (rx == 0) begin
-                start_baud = 1;
-            end
-        end
+                    if (data_counter == 16) begin
+                        data_counter = 0;
+                        state = STOP;
+                    end  
+                end
+                
+            STOP : begin
+                    data_counter = data_counter + 1;
+                    if (data_counter == 4) begin
+                        data_counter = 0;
+                        state = IDLE;
+                    end 
+                end
+        
+        endcase
+        
     end
 endmodule
 
--- UART/top_tb.sv
+++ UART/top_tb.sv
@@ -15,6 +15,7 @@ endinterface
 
 module test(intf_object.DUT intf);
 
+    typedef enum {IDLE, START, DATA, PAR, STOP} uart_states;
   
     logic data_arr[8:0];
     int data_counter;
@@ -22,44 +23,54 @@ module test(intf_object.DUT intf);
     logic parity_baud;
     logic stop_baud[1:0];
     
+    
+    uart_states state;
+    
     uart dut (.rx(intf.cb.rx), .tx(intf.cb.tx));
     logic send_bit;
-	initial begin // sequential block
-        send_bit = 1;
-        intf.cb.rx <= 1;
-        data_counter = 0;
-	end
+    
     always @ (intf.cb) begin
-        //intf.cb.rx <= ~send_bit;
-        //intf.cb.rx <= 0;
-        send_bit <= ~send_bit;
-        if (start_baud == 1) begin
-            intf.cb.rx <= send_bit;
-            send_bit = ~send_bit;
+        
+        case (state)
+            IDLE : begin
+                    send_bit = 1;
+                    intf.cb.rx <= 1;
+                    data_counter = 0;
+                    state = START;
+                end
+                
+            START : begin
+                    intf.cb.rx <= 0;
+                    state = DATA;
+                end
+                
+            DATA : begin
+                    intf.cb.rx <= send_bit;
+                    send_bit = ~send_bit;
+                    
+                    data_counter = data_counter + 1;
+                    if (data_counter == 8) begin
+                        data_counter = 0;
+                        state = STOP;
+                    end
             
-            data_counter = data_counter + 1;
-            if (data_counter == 8) begin
-                start_baud = 0;
-                data_counter = 9;
-                intf.cb.rx <= 1;
-            end
-            //start_baud = 0;
-        end
-        
-        else if (data_counter == 9) begin
-            parity_baud = 1;
-        end
-        
-        else begin
-            start_baud = 1;
-            intf.cb.rx <= 0;
-        end
+                end
+                
+            STOP : begin
+                    intf.cb.rx <= 1;
+                    data_counter = data_counter + 1;
+                    if (data_counter == 2) begin
+                        data_counter = 0;
+                        state = IDLE;
+                    end
+                end
+        endcase
     end
 
 
 endmodule : test
 
-module testbench(); // Testbench has no inputs, outputs
+module testbench();
 
 
     bit clk;
